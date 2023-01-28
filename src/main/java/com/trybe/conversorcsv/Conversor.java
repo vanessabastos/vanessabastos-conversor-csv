b86ed9e5c3d27fad90079f0c7828bd77dfa1b2da
vanessabastos <vanessarsb83@gmail.com>
2023-01-28 20:12:17 -0300

    Iniciando desafio Conversor CSV

--- src/main/java/com/trybe/conversorcsv/Conversor.java
+++ src/main/java/com/trybe/conversorcsv/Conversor.java
@@ -1,17 +1,15 @@
 package com.trybe.conversorcsv;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
+/**Classe conversor.*/
 public class Conversor {
 
-  /**
-   * Função utilizada apenas para validação da solução do desafio.
-   *
-   * @param args Não utilizado.
-   * @throws IOException Caso ocorra algum problema ao ler os arquivos de entrada ou
-   *                     gravar os arquivos de saída.
-   */
   public static void main(String[] args) throws IOException {
     File pastaDeEntradas = new File("./entradas/");
     File pastaDeSaidas = new File("./saidas/");
@@ -30,6 +28,58 @@ public class Conversor {
    *                     gravar os arquivos de saída.
    */
   public void converterPasta(File pastaDeEntradas, File pastaDeSaidas) throws IOException {
-    // TODO: Implementar.
+    File pastaSaida = null;
+    FileReader leitorArquivo = null;
+    FileWriter escritorArquivo = null;
+    BufferedReader bufferedLeitor = null;
+    BufferedWriter bufferedEscritor = null;
+
+    if (!pastaDeSaidas.exists()) {
+      pastaDeSaidas.mkdir();
+    }
+
+    if (pastaDeEntradas.isDirectory() && pastaDeEntradas.canRead()) {
+      for (File arquivo : pastaDeEntradas.listFiles()) {
+        leitorArquivo = new FileReader(arquivo);
+        bufferedLeitor = new BufferedReader(leitorArquivo);
+        pastaSaida = new File(pastaDeSaidas + "/" + arquivo.getName());
+        escritorArquivo = new FileWriter(pastaSaida);
+        bufferedEscritor = new BufferedWriter(escritorArquivo);
+
+        String informacoesLinha = bufferedLeitor.readLine();
+
+        while (informacoesLinha != null) {
+          if (!informacoesLinha.contains("Nome Completo")) {
+            String linhaEditada = editarLinha(informacoesLinha);
+            bufferedEscritor.write(linhaEditada);
+            bufferedEscritor.newLine();
+            bufferedEscritor.flush();
+          } else {
+            bufferedEscritor.write(informacoesLinha);
+            bufferedEscritor.newLine();
+            bufferedEscritor.flush();
+          }
+          informacoesLinha = bufferedLeitor.readLine();
+        }
+      }
+    }
+    leitorArquivo.close();
+    bufferedLeitor.close();
+    escritorArquivo.close();
+    bufferedEscritor.close();
+  }
+
+  private String editarLinha(String informacoesLinha) {
+    String[] informacoesSeparadas = informacoesLinha.split(",");
+    String nome = informacoesSeparadas[0].toUpperCase();
+    String[] data = informacoesSeparadas[1].split("/");
+    String dataFormatada = data[2] + "-" + data[1] + "-" + data[0];
+    String email = informacoesSeparadas[2];
+    String cpf = informacoesSeparadas[3]
+        .replaceAll("^(\\d{3})(\\d{3})(\\d{3})(\\d{2})", "$1.$2.$3-$4");
+
+    String formatoLinha = nome + "," + dataFormatada + "," + email + "," + cpf;
+    
+    return formatoLinha; 
   }
 }
